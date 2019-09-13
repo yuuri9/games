98c5546d4bdb25a05e7f2b5d328c247dac3fc7f5
VIPPER <kuro@mail.fufufu.moe>
2019-09-13 04:47:43 +0000

    Gui demo keys

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ main(int argc, char** argv){
 
 	UIface UIface;
 
-	GEntity GEntity;
+	GEntity GEntity, FEntity, *selentity;
 
 	Settings.seed = 0;
 	/*PRECEDENCE: File seed overwrites cli seed*/
@@ -96,14 +96,17 @@ main(int argc, char** argv){
 	einit(Ekeyboard|Emouse);
 	etimer(Etimer, trate);
 
- 	initentity(&GEntity, 240, 2, 2, 2, tileset, display);
- 
+ 	initentity(&GEntity, 2569, 2, 2, 2, tileset, display);
+ 	initentity(&FEntity, 2570, 2,2,2,tileset, display);
+
+	selentity = &GEntity;
+
 	for(gclock=0;++gclock;){
 		draw(screen, Rect(screen->r.min.x, screen->r.min.y, screen->r.max.x, screen->r.max.y), bground, nil, ZP);
  		drawentity(&GEntity, screen, 128, 128);
- 
-		drawui(&UIface, screen, &GEntity, 32 , 32);
-		/*draw(screen, Rect(screen->r.min.x + 17, screen->r.min.y + 15, screen->r.min.x + 49, screen->r.min.y + 47), GStack[0].tile, nil, ZP);*/
+ 		drawentity(&FEntity, screen, 400,400);
+
+		drawui(&UIface, screen, selentity, 32 , 32);
 
 
 
@@ -123,21 +126,62 @@ main(int argc, char** argv){
 
 			}
 			if(Event.kbdc == 'E' || Event.kbdc == 'e'){
-				++GEntity.hp;
-				++GEntity.mhp;
-				++GEntity.mp;
-				++GEntity.mmp;
-				++GEntity.exp;
-				++GEntity.mexp;
+				++selentity->hp;
+				++selentity->mhp;
+				++selentity->mp;
+				++selentity->mmp;
+				++selentity->exp;
+				++selentity->mexp;
+			}
+			if(Event.kbdc == 'f' || Event.kbdc == 'F'){
+				++selentity->mhp;
+				++selentity->mmp;
+				++selentity->mexp;
+
+
+			}
+			if(Event.kbdc == 'g' || Event.kbdc == 'G'){
+				++selentity->hp;
+				++selentity->mp;
+				++selentity->exp;
+
+
+			}
+			if(Event.kbdc == 'K' || Event.kbdc == 'k'){
+				--selentity->hp;
+				--selentity->mhp;
+				--selentity->mp;
+				--selentity->mmp;
+				--selentity->exp;
+				--selentity->mexp;
+			}
+			if(Event.kbdc == 'h' || Event.kbdc == 'H'){
+				--selentity->mhp;
+				--selentity->mmp;
+				--selentity->mexp;
+
+
+			}
+			if(Event.kbdc == 's' || Event.kbdc == 'S'){
+				--selentity->hp;
+				--selentity->mp;
+				--selentity->exp;
+
+
 			}
 
-
 		}
+
 		if(Key == Etimer){
 
 		}
 		if(Key == Emouse){
-
+			if((Event.mouse.buttons & 1) == 1 && Event.mouse.xy.x > screen->r.min.x + 128 && Event.mouse.xy.y > screen->r.min.y + 128 && Event.mouse.xy.y < (screen->r.min.y + 128 + 32) && Event.mouse.xy.x < (screen->r.min.x + 128+32)){
+				selentity = &GEntity;
+			}
+			if((Event.mouse.buttons & 1) == 1 && Event.mouse.xy.x > screen->r.min.x + 400 && Event.mouse.xy.y > screen->r.min.y + 400 && Event.mouse.xy.y < (screen->r.min.y + 400 + 32) && Event.mouse.xy.x < (screen->r.min.x + 400+32)){
+				selentity = &FEntity;
+			}
 		}
 
 
